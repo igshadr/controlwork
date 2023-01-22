4dcebc6f14a6aca9186c8f975c068738ea601f1c
IgorShadrin <ig-shadr@yandex.ru>
2023-01-22 18:01:00 +0300

    chanched code

--- Program.cs
+++ Program.cs
@@ -9,27 +9,60 @@
 
 using System;
 
-string[] arr={"Hello", "2", "world", ":-)"};
+Console.WriteLine("Введите через запятую строки формируемого массива:");
+//примеры для ввода с клавиатуры
+//Hello,2,world,:-)
+//1234,1567,-2,computer science
+//Russia,Denmark,Kazan
 
-//вывод массива на экран
-Console.Write($"Ввели массив: ");
-for(int i=0;i<arr.Length;i++)
+string? text=Console.ReadLine();
+if(!string.IsNullOrEmpty(text))
 {
-      Console.Write($"\"{arr[i]}\" ");
-      if(i<arr.Length-1)
-            Console.Write($", ");
+      string[] arr={};
+      arr=text.Split(',');
+      //вывод массива на экран
+      Console.Write($"Ввели массив: ");
+      PrintArr(arr); 
+   
+      //формирование нового массива из строк длиной <=3 символов
+      string[] arrnew={};
+      CreateNewArray(ref arr, ref arrnew);
+
+      //вывод результата на экран
+      Console.Write($"Новый массив: ");
+      PrintArr(arrnew);
+}
+else
+      Console.WriteLine("Введена пустая строка.");
+
+
+//дополнительные функции
+//печать массива
+void PrintArr(string[] a)
+{
+      if(a.Length>=0)
+            Console.Write($"[ ");
+      
+      for(int i=0;i<a.Length;i++)
+      {
+            Console.Write($"\"{a[i]}\" ");
+            if(i<a.Length-1)
+                  Console.Write($", ");
+      }
+      
+      if(a.Length>=0)
+            Console.WriteLine($"]");
 }
 
-//формирование нового массива из строк длиной <=3 символов
-string[] arrnew=arr;
-
-//вывод результата на экран
-Console.Write($"Новый массив: ");
-for(int i=0;i<arrnew.Length;i++)
+//формирование нового массива
+void CreateNewArray(ref string[] s, ref string[] snew)
 {
-      Console.Write($"\"{arrnew[i]}\" ");
-      if(i<arrnew.Length-1)
-            Console.Write($", ");
+      for(int i=0;i<s.Length;i++)
+      {
+            if(s[i].Length<=3)
+            {
+                  Array.Resize(ref snew, snew.Length+1);
+                  snew[snew.Length-1]=s[i];
+            }
+      }
 }
-
-
